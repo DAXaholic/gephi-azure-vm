57dd148168fad9f2000b3bd771b1970fa1b186a2
DAXaholic <me@daxaholic.com>
2019-02-17 18:11:17 +0100

    Run initialization script under admin account

--- Init.ps1
+++ Init.ps1
@@ -1,16 +1,31 @@
-# Install Chocolatey
-Invoke-Expression
-    (Invoke-WebRequest 'https://chocolatey.org/install.ps1').Content
+param(
+    $VmAdminUserName,
+    $VmAdminPassword
+)
 
-# Install Java
-choco.exe install javaruntime -y
+$adminPasswordSecStr = ConvertTo-SecureString -AsPlainText -Force $VmAdminPassword
+$credentials = [System.Management.Automation.PSCredential]::new(
+    $VmAdminUserName,
+    $adminPasswordSecStr)
 
-# Download Gephi Installer
-$invokeWebReqArgs = @{
-    Uri = 'https://github.com/gephi/gephi/releases/download/v0.9.2/gephi-0.9.2-windows.exe'
-    OutFile = 'gephi.exe'
+Invoke-Command -Credential $credentials -ComputerName $env:COMPUTERNAME -ScriptBlock {
+
+    # Install Chocolatey
+    Invoke-Expression (Invoke-WebRequest 'https://chocolatey.org/install.ps1').Content
+
+    # Install Java
+    choco.exe install javaruntime -y
+
+    # Download Gephi Installer
+    $tempPath = [IO.Path]::GetTempPath()
+    $tempDir = Join-Path -Path $tempPath -ChildPath 'GephiDownload'
+    $tempExe = Join-Path -Path $tempDir -ChildPath 'gephi.exe'
+    $invokeWebReqArgs = @{
+        Uri     = 'https://github.com/gephi/gephi/releases/download/v0.9.2/gephi-0.9.2-windows.exe'
+        OutFile = $tempExe
+    }
+    Invoke-WebRequest @invokeWebReqArgs
+
+    # Install Gephi
+    & $tempExe '/SILENT' | Out-Null
 }
-Invoke-WebRequest @invokeWebReqArgs
-
-# Install Gephi
-.\gephi.exe /SILENT
